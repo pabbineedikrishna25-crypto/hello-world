d368a7399ecd155ec93f6b6121e3efdb670482da
pabbineedikrishna25-crypto <pabbineedikrishna25@gmail.com>
2025-11-11 22:17:29 +0530

    Add files via upload

---
+++ area sample triangle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	float breadth,height,area;
+	printf("enter breadth&height values ::");
+	scanf("%f %f",&breadth,&height);
+	area=0.5*breadth*height;
+	printf("area=%f",area);
+	return 0;
+	
+}
