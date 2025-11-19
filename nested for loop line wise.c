e450c5e9aaae195749e02f8d3c2080f126025191
pabbineedikrishna25-crypto <pabbineedikrishna25@gmail.com>
2025-11-19 05:57:22 +0530

    Add files via upload

---
+++ nested for loop line wise.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int i;
+  {
+   for(i=1;i<=5;i++)
+	printf(" %d",i);
+	printf("\n");
+	for(i=1;i<=4;i++)
+	printf(" %d",i);
+	printf(" \n");
+	for(i=1;i<=3;i++)
+	printf(" %d",i);
+	printf(" \n");
+	for(i=1;i<=2;i++)
+    printf(" %d",i);
+	printf(" \n");
+	for(i=1;i<=1;i++)
+	printf(" %d",i);
+	return 0;
+}
+	
+}
