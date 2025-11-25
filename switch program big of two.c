db135caf70ffedb2d2ccf241ebf2b3fc692e1b8c
pabbineedikrishna25-crypto <pabbineedikrishna25@gmail.com>
2025-11-25 16:10:50 +0530

    Add files via upload

---
+++ switch program big of two.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter a&b values");
+	scanf("%d %d",&a,&b);
+	switch(a>b)
+	case 1:printf("a is big");
+	                                                                                                                                break;
+     case 0:printf(" bis big");
+            break;
+    default:printf("a=b");
+    return 0;
+}
